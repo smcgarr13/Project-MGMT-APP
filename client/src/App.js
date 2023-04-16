95b420599cc1ea3c563230efc26bd3705a3221a4
Sparkles <smcgarr13@yahoo.com>
2023-04-16 03:52:57 -0500

    addind initial front end files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache }
+from '@apollo/client';
+import Client from './components/Clients';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <>
+    <ApolloProvider client={client}>
+    <Header />
     <div className="container">
       <h1>Hello World!!</h1>
-     
     </div>
+    </ApolloProvider>
+    </>
   );
 }
 
