baf998137632893a19ab5656bb8dac564ee6ed8f
Sparkles <smcgarr13@yahoo.com>
2023-04-16 19:35:38 -0500

    front-end-updates-1

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,46 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import { ApolloProvider, ApolloClient, InMemoryCache }
-from '@apollo/client';
-import Client from './components/Clients';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Clients from './components/Clients';
+import Projects from './components/Projects';
+import AddClientModal from './components/AddClientModal';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+      clients: {
+        merge(existing, incoming) {
+          return incoming;
+        }
+      },
+      projects: {
+        merge(existing, incoming) {
+          return incoming;
+        }
+      }
+    }
+    }
+  }
+})
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  uri: 'http://localhost:3001/graphql',
+  cache,
 });
 
 function App() {
   return (
     <>
     <ApolloProvider client={client}>
+      <Router>
     <Header />
-    <div className="container">
-      <h1>Hello World!!</h1>
+    <div className='container'>
+      <AddClientModal />
+      <Projects />
+      <Clients />
     </div>
+    </Router>
     </ApolloProvider>
     </>
   );
